6d4ad6d7219b296a82b6b77489d591260ad9d00c
Nguyễn Tuấn Minh <167443836+sirMatras@users.noreply.github.com>
2025-02-13 14:43:11 +0300

    Add files via upload

---
+++ KPO/KPO/VeterinaryClinic.cs
@@ -0,0 +1,37 @@
+﻿namespace KPO;
+
+public interface IHealthCheck
+{
+    bool CheckHealth(Animal newAnimal);
+}
+ 
+public interface ICheckKindness
+{
+    bool CheckKindness(Herbo newAnimal);
+}
+public class VeterinaryClinic : IHealthCheck, ICheckKindness
+{
+    public bool CheckHealth(Animal newAnimal)
+    {
+        if (newAnimal == null)
+        {
+            return false;
+        }
+
+        return newAnimal.HealthLevel >= 5;
+    }
+
+    public bool CheckKindness(Herbo newAnimal)
+    {
+        if (newAnimal.KindnessLevel > 5)
+        {
+            Console.WriteLine($"{newAnimal.Name} добрый. Уровень доброты - {newAnimal.KindnessLevel}.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"{newAnimal.Name} злой. Уровень доброты - {newAnimal.KindnessLevel}.");
+            return false;
+        }
+    }
+}
